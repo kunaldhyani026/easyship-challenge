c8b82de443f61ec058c12f02fd519fe7b5acbe1a
Kunal <kunal@pramata.com>
2024-02-18 00:43:45 +0530

    task1-display_items_included_in_shipment : code added for shipment instance method to group associated shipment_items by description and order by their count

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,30 @@
 class Shipment < ApplicationRecord
   belongs_to :companies
   has_many :shipment_items
+
+  # Groups the shipment items by description and returns an array of hashes representing
+  # each description along with the count of items with that description. The array is
+  # sorted based on the count of items in either ascending or descending order based on items_order param.
+  # Parameters:
+  # - items_order: A string indicating the order of sorting ('ascending' or 'descending').
+  # Returns: An array of hashes, each hash containing the description and count of items.
+  def group_shipment_items(items_order)
+    order_clause = determine_order_clause(items_order)
+    grouped_items = shipment_items.group(:description).select('description, COUNT(*) as count')
+    grouped_items = grouped_items.order("count #{order_clause}") if order_clause.present?
+    grouped_items.map { |item| { description: item.description, count: item.count } }
+  end
+
+  private
+
+  # Determines the order clause based on the items_order parameter.
+  # Parameters:
+  # - items_order: A string indicating the order of sorting ('ascending' or 'descending').
+  # Returns: A string representing the order clause ('ASC' or 'DESC') or an empty string if no sorting is needed.
+  def determine_order_clause(items_order)
+    return 'ASC' if items_order == 'ascending'
+    return 'DESC' if items_order == 'descending'
+
+    ''
+  end
 end
